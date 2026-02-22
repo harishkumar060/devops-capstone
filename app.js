1397394ee058dcc8831cfcff27cf7d3f4add3f12
Harish Kumar <harishkumar060@gmail.com>
2026-02-22 11:54:31 +0530

    Update response message for root route

--- app.js
+++ app.js
@@ -3,18 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head><title>DevOps Capstone</title></head>
-      <body style="text-align: center; font-family: sans-serif;">
-        <h1>🚀 Continuous Delivery Pipeline Active</h1>
-        <p>Node.js App is running inside a Docker Container on AWS EC2.</p>
-        <p>Status: <b>Healthy</b></p>
-      </body>
-    </html>
-  `);
+  res.send('<h1>Capstone Project Successfully Deployed!</h1><p>Running on Docker via Jenkins Pipeline.</p>');
 });
 
 app.listen(port, () => {
-  console.log(`Application listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
